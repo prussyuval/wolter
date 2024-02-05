e8996b2e7cededbb43a02025cc9b2f7e21bde5c0
yuval <prussyuval@gmail.com>
2024-02-05 18:47:46 +0200

    Update wolter

--- wolter.py
+++ wolter.py
@@ -1,9 +1,9 @@
+import os
 import requests
 import time
-import winsound
 from typing import List
 
-SEARCH_URL = "https://restaurant-api.wolt.com/v1/search"
+SEARCH_URL = "https://restaurant-api.wolt.com/v1/pages/search"
 INTERVAL_TIME_SEC = 10
 VOICE_FREQUENCY = 2500  # Set Frequency To 2500 Hertz
 VOICE_DURATION = 500  # Set Duration To 1000 ms == 1 second
@@ -16,18 +16,28 @@ def search_restaurant(name: str) -> List:
                                                     lat=32.087236876497585,
                                                     lon=34.78698525756491))
     json_response = response.json()
-    return json_response.get('results', [])
+    sections = json_response.get('sections', [])
+    if len(sections) == 0:
+        return []
+
+    return sections[0]["items"]
 
 
-def is_opened(name: str, identifier: str) -> bool:
+def is_opened(name: str, public_url: str) -> bool:
     results = search_restaurant(name)
 
-    for result in results:
-        if result['value']['id']['$oid'] == identifier:
-            return result['value']['online']
+    relevant = None
 
-    print('Restaurant name disappeared (may due to brute force restrictions)!')
-    return False
+    for result in results:
+        if public_url == result['link']['target']:
+            relevant = result
+            break
+        
+    if relevant is None:
+        print('Restaurant name disappeared (may due to brute force restrictions)!')
+        return False
+
+    return relevant.get('venue', {}).get('online', False)
 
 
 if __name__ == '__main__':
@@ -39,22 +49,19 @@ if __name__ == '__main__':
         exit(1)
 
     for i, result in enumerate(results, start=1):
-        result_value = result['value']
-        full_name = result_value['name'][0]['value']
-        address = result_value['address']
-        print(f'[{i}]\t{full_name}\t{address}')
+        title = result['title']
+        print(f'[{i}]\t{title}')
 
     restaurant_index = int(input("Enter the requested restaurant id: "))
 
     restaurant_data = results[restaurant_index - 1]
-    restaurant_id = restaurant_data['value']['id']['$oid']
-    public_url = restaurant_data['value']['public_url']
+    public_url = restaurant_data['link']['target']
 
-    while not is_opened(restaurant_name, restaurant_id):
+    while not is_opened(restaurant_name, public_url):
         print(f"Restaurant is closed, sleeping for {INTERVAL_TIME_SEC} seconds!")
         time.sleep(INTERVAL_TIME_SEC)
 
     print(f"Restaurant is open! visit: {public_url}")
     while True:
-        winsound.Beep(VOICE_FREQUENCY, VOICE_DURATION)
+        os.system('say "Ready to order!"')
         time.sleep(1)
